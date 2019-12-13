90df5c9abe99114444c2e7fc1505f84598351b40
MrityunjaiKumar <mrityunjai.kmr@gmail.com>
2019-12-13 18:25:23 +0530

    files moved

--- Rtc_Pcf8563.h
+++ Rtc_Pcf8563.h
@@ -114,6 +114,39 @@
 #define SQW_32HZ        B10000010
 #define SQW_1HZ         B10000011
 
+
+// Parse the __DATE__ predefined macro to generate date defaults:
+// __Date__ Format: MMM DD YYYY (First D may be a space if <10)
+// <MONTH>
+#define BUILD_MONTH_JAN ((__DATE__[0] == 'J') && (__DATE__[1] == 'a')) ? 1 : 0
+#define BUILD_MONTH_FEB (__DATE__[0] == 'F') ? 2 : 0
+#define BUILD_MONTH_MAR ((__DATE__[0] == 'M') && (__DATE__[1] == 'a') && (__DATE__[2] == 'r')) ? 3 : 0
+#define BUILD_MONTH_APR ((__DATE__[0] == 'A') && (__DATE__[1] == 'p')) ? 4 : 0
+#define BUILD_MONTH_MAY ((__DATE__[0] == 'M') && (__DATE__[1] == 'a') && (__DATE__[2] == 'y')) ? 5 : 0
+#define BUILD_MONTH_JUN ((__DATE__[0] == 'J') && (__DATE__[1] == 'u') && (__DATE__[2] == 'n')) ? 6 : 0
+#define BUILD_MONTH_JUL ((__DATE__[0] == 'J') && (__DATE__[1] == 'u') && (__DATE__[2] == 'l')) ? 7 : 0
+#define BUILD_MONTH_AUG ((__DATE__[0] == 'A') && (__DATE__[1] == 'u')) ? 8 : 0
+#define BUILD_MONTH_SEP (__DATE__[0] == 'S') ? 9 : 0
+#define BUILD_MONTH_OCT (__DATE__[0] == 'O') ? 10 : 0
+#define BUILD_MONTH_NOV (__DATE__[0] == 'N') ? 11 : 0
+#define BUILD_MONTH_DEC (__DATE__[0] == 'D') ? 12 : 0
+#define BUILD_MONTH BUILD_MONTH_JAN | BUILD_MONTH_FEB | BUILD_MONTH_MAR |     \
+                        BUILD_MONTH_APR | BUILD_MONTH_MAY | BUILD_MONTH_JUN | \
+                        BUILD_MONTH_JUL | BUILD_MONTH_AUG | BUILD_MONTH_SEP | \
+                        BUILD_MONTH_OCT | BUILD_MONTH_NOV | BUILD_MONTH_DEC
+// <DATE>
+#define BUILD_DATE_0 ((__DATE__[4] == ' ') ? 0 : (__DATE__[4] - 0x30))
+#define BUILD_DATE_1 (__DATE__[5] - 0x30)
+#define BUILD_DATE ((BUILD_DATE_0 * 10) + BUILD_DATE_1)
+// <YEAR>
+#define BUILD_YEAR (((__DATE__[7] - 0x30) * 1000) + ((__DATE__[8] - 0x30) * 100) + \
+                    ((__DATE__[9] - 0x30) * 10) + ((__DATE__[10] - 0x30) * 1))
+
+#define BUILD_HOUR ((__TIME__[0] - '0') * 10 + __TIME__[1] - '0')
+#define BUILD_MIN ((__TIME__[3] - '0') * 10 + __TIME__[4] - '0')
+#define BUILD_SEC ((__TIME__[6] - '0') * 10 + __TIME__[7] - '0')
+
+
 #include <Arduino.h>
 #include <Wire.h>
 
